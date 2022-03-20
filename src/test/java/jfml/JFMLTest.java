883453694379fce7998a6e27e0030e024688fd38
F-Vigo <49415023+F-Vigo@users.noreply.github.com>
2022-03-20 15:26:29 +0100

    Former duplicated files removed, and JUnit added

---
+++ src/test/java/jfml/JFMLTest.java
@@ -0,0 +1,16 @@
+package jfml;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class JFMLTest {
+
+    @Test
+    public void checkTest() {
+        // Si la línea siguiente se descomenta, el test falla.
+        // Al hacer mvn clean install se lanzan todos los test, de modo que también fallaría.
+        // fail("This was supposed to happen :).");
+    }
+}
