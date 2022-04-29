d367f613b05fe9a00fe246999e49b80d07d180af
F-Vigo <49415023+F-Vigo@users.noreply.github.com>
2022-04-29 11:56:11 +0200

    Test cleaned

--- src/test/java/jfml/JFMLTest.java
+++ src/test/java/jfml/JFMLTest.java
@@ -8,9 +8,5 @@ import static org.junit.Assert.fail;
 public class JFMLTest {
 
     @Test
-    public void checkTest() {
-        // Si la línea siguiente se descomenta, el test falla.
-        // Al hacer mvn clean install se lanzan todos los test, de modo que también fallaría.
-        // fail("This was supposed to happen :).");
-    }
+    public void checkTest() {}
 }
